b3379c7c5037015093ab6592909fca16e364a1c9
SilentSage <tys2157787@maricopa.edu>
2025-11-18 15:26:49 -0700

    Fix router basename for dev vs production

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,3 +1,4 @@
+/// <reference types="vite/client" />
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -6,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/ui">
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/ui"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
